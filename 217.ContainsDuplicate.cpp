73f1618787968d84845e7c72d6e3de5435778393
ravimusunuri <58125335+ravimusunuri@users.noreply.github.com>
2019-11-27 21:45:18 -0800

    Create 217.ContainsDuplicate.cpp

---
+++ 217.ContainsDuplicate.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_set<int> myset;
+        
+        for (int i=0;i<nums.size();i++){
+            if (myset.find(nums[i]) == myset.end()){
+                myset.insert(nums[i]);
+            } else {
+                return true;
+            }
+        }
+        return false;
+    }
+};
