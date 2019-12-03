a8a87316c1943686f46809f281ff382644aad57a
ravimusunuri <58125335+ravimusunuri@users.noreply.github.com>
2019-12-02 21:07:00 -0800

    Update 238. Product of Array Except Self.cpp

--- 238. Product of Array Except Self.cpp
+++ 238. Product of Array Except Self.cpp
@@ -1,28 +1,52 @@
 class Solution {
 public:
-    vector<int> productExceptSelf(vector<int>& nums) {
     
-      vector<int> left(nums.size());
-      vector<int> right(nums.size());
-      vector<int> res(nums.size());
-        
-      if (nums.size() == 0)
-          return res;
-        
-      left[0] = 1;
-      for (int i=1; i< nums.size(); i++) {
-          left[i] = left[i-1] *nums[i-1];
-      }
+      struct Node {
+        int left_size;
+        int node_size;
+        int val;
+        Node * left;
+        Node * right;
+        Node (int n) :
+            val(n),
+            node_size(1),
+            left_size(0),
+            left(NULL),
+            right(NULL)
+        {}
+    };
     
-      right[nums.size()-1] = 1;
-      for (int i= nums.size()-2; i>=0; i--) {
-          right[i]  =right[i+1] *nums[i+1];
-      }
-      
-      for (int i=0; i< nums.size(); i++) {
-          res[i] = left[i]*right[i];
-      }
-        
-      return res;
+    
+    
+    Node* insert(Node *root, int key, int* count) {
+        if (!root) {
+            return (new Node(key));
+        } else if (root->val == key) {
+            root->node_size++;
+            *count = *count+ root->left_size;
+        } else if (key < root->val) {
+            root->left_size++;
+            root->left = insert(root->left, key, count);
+        } else {
+            *count = *count+ root->left_size+root->node_size;
+            root->right = insert(root->right, key, count);
+        }
+        return root;
     }
+    
+    
+    vector<int> countSmaller(vector<int>& nums) {
+        if (nums.size() == 0) {
+            return {};
+        } else if (nums.size() == 1) {
+            return {0};
+        }
+        vector<int> count(nums.size());
+        Node * root = NULL;
+        for (int i = nums.size() - 1; i >= 0; i--) {
+            root = insert(root, nums[i],&count[i]);
+        }
+        return count;
+    }
+    
 };
