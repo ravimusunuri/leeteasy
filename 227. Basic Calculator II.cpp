605c546e56117c958fb33b26ba5d9a2c114083e3
ravimusunuri <58125335+ravimusunuri@users.noreply.github.com>
2019-12-04 14:47:22 -0800

    Create 227. Basic Calculator II.cpp

---
+++ 227. Basic Calculator II.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    
+
+// O(N) time O(1) space
+int calculate(string s) {
+        stringstream ss("+" + s);
+        char op;
+        int n, last, ans = 0;
+        while (ss >> op >> n) {
+            if (op == '+' || op == '-') {
+                n = op == '+' ? n : -n;
+                ans += n;
+            } else {
+                n = op == '*' ? last * n : last / n;
+                ans = ans - last + n; // simulate a stack by recovering last values
+            }
+            last = n;
+        }
+        return ans;
+}
+    
+};    
+    
